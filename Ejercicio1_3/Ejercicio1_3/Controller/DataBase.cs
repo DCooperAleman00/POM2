fe4bc62204608413f9210f73f4615d03877682d6
danny <danny@DESKTOP-PIUU10N>
2022-10-21 22:26:27 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_3/Ejercicio1_3/Controller/DataBase.cs
@@ -0,0 +1,55 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ejercicio1_3.Models;
+
+namespace Ejercicio1_3.Controller
+{
+ 
+    public class DataBase
+    {
+        readonly SQLiteAsyncConnection dbase;
+
+        //creacion de BD
+        public DataBase(string dbpath)
+        {
+            dbase = new SQLiteAsyncConnection(dbpath);
+            dbase.CreateTableAsync<Models.Personas>(); 
+        }
+
+        //crud
+        #region OperacionesPersona
+        public Task<int> GuardarPersona(Personas persona)
+        {
+            if (persona.id != 0)
+            {
+                return dbase.UpdateAsync(persona);
+            }
+            else
+            {
+                return dbase.InsertAsync(persona);
+            }
+
+        }  
+
+        //mostrar personas
+         public Task<List<Personas>> getListPersonas()
+        {
+            return dbase.Table<Personas>().ToListAsync();
+        }
+        //mostrar una persona
+        public Task<Personas> getPersona(int pid)
+        {
+            return dbase.Table<Personas>() 
+                .Where(i => i.id == pid)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<int> DeletePersona(Personas persona)
+        {
+            return dbase.DeleteAsync(persona);
+        }
+        #endregion OperacionesPersona
+
+    }
+}
---
+++ Ejercicio1_3/Ejercicio1_3/Models/Personas.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_3.Models
+{
+    public class Personas
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string edad { get; set; }
+        public string correo { get; set; }
+        public string direccion { get; set; }
+    }
+}
